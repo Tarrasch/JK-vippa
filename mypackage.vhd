94d07175208d47941fc69b782c831cc0a84d1da5
Arash Rouhani <miffoljud@gmail.com>
2011-02-20 19:08:19 +0100

    Added entity for package file

--- mypackage.vhd
+++ mypackage.vhd
@@ -7,5 +7,12 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
 
-
+package mypackage is
+  component fjr
+    port (
+       clk, reset, j, k : IN std_logic;
+       q                : OUT std_logic
+      ) ;
+  end component;
+end package; -- mypackage
 
