8ca03b4c7403f07871046b8f30a86efe13d10955
Arash Rouhani <miffoljud@gmail.com>
2011-02-20 19:05:50 +0100

    Added imports for package file

--- fjr.vhd
+++ fjr.vhd
@@ -23,12 +23,10 @@ begin
     
     if clk'Event and clk = '1' then
       -- only take action when rising edge
-
       if reset = '1' then
         -- reset is on
           v := '0';
       elsif j = '1' and k = '1' then
-        -- reset is off and count is on
           v := not v;
       elsif j = '1' then
           v := '1';
---
+++ mypackage.vhd
@@ -0,0 +1,11 @@
+-- Arash Rouhani
+-- cid: rarash
+-- inlamningsuppgift 3.2
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+
+
+
+
