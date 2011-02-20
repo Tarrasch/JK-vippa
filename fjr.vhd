034f117c4192a37c7f2d230904c3d0bd6695d524
Arash Rouhani <miffoljud@gmail.com>
2011-02-20 18:57:52 +0100

    added parts of architecture

--- fjr.vhd
+++ fjr.vhd
@@ -7,3 +7,23 @@ entity fjr is
 end entity;
 
 
+
+architecture mybcd23 of bcd23 is
+begin
+  p0 : process(clk)
+    variable v :std_logic;
+  begin
+    
+    if clk'Event and clk = '1' then
+      -- only take action when rising edge
+
+      if reset = '1' then
+        -- reset is on
+          v = 0;
+      elsif j = '1' and k = '1' then
+        -- reset is off and count is on
+          v = not v;
+      end if;
+    end if;
+  end process;
+end architecture;
